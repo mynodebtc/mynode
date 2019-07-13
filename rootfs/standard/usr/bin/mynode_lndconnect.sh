752e8dbfd6f510ede75a2e55cc4c9d813da46d1c
mynode-dev <dev@mynodebtc.com>
2019-07-13 18:56:42 -0500

    Remove LND Connect Remote (adding VPN later)

--- rootfs/standard/usr/bin/mynode_lndconnect.sh
+++ rootfs/standard/usr/bin/mynode_lndconnect.sh
@@ -18,19 +18,19 @@ while true; do
     rm -rf /tmp/mynode_lndconnect/*
 
     # Generate QR Codes
-    lndconnect --lnddir=/mnt/hdd/mynode/lnd -o --bitcoin.mainnet
-    cp -f lndconnect-qr.png lndconnect_remote_grpc.png
+    #lndconnect --lnddir=/mnt/hdd/mynode/lnd -o --bitcoin.mainnet
+    #cp -f lndconnect-qr.png lndconnect_remote_grpc.png
     lndconnect --lnddir=/mnt/hdd/mynode/lnd -o --bitcoin.mainnet --localip
     cp -f lndconnect-qr.png lndconnect_local_grpc.png
-    lndconnect --lnddir=/mnt/hdd/mynode/lnd -o --bitcoin.mainnet -p 10080
-    cp -f lndconnect-qr.png lndconnect_remote_rest.png
+    #lndconnect --lnddir=/mnt/hdd/mynode/lnd -o --bitcoin.mainnet -p 10080
+    #cp -f lndconnect-qr.png lndconnect_remote_rest.png
     lndconnect --lnddir=/mnt/hdd/mynode/lnd -o --bitcoin.mainnet --localip -p 10080
     cp -f lndconnect-qr.png lndconnect_local_rest.png
 
     # Generate Text Files
-    lndconnect --lnddir=/mnt/hdd/mynode/lnd -j --bitcoin.mainnet > lndconnect_remote_grpc.txt
+    #lndconnect --lnddir=/mnt/hdd/mynode/lnd -j --bitcoin.mainnet > lndconnect_remote_grpc.txt
     lndconnect --lnddir=/mnt/hdd/mynode/lnd -j --bitcoin.mainnet --localip > lndconnect_local_grpc.txt
-    lndconnect --lnddir=/mnt/hdd/mynode/lnd -j --bitcoin.mainnet -p 10080 > lndconnect_remote_rest.txt
+    #lndconnect --lnddir=/mnt/hdd/mynode/lnd -j --bitcoin.mainnet -p 10080 > lndconnect_remote_rest.txt
     lndconnect --lnddir=/mnt/hdd/mynode/lnd -j --bitcoin.mainnet --localip -p 10080 > lndconnect_local_rest.txt
 
     echo "Done! Waiting until LND changes, then regen lndconnect codes!"
