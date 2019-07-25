2842c0ab86c80ab8675c781af8fd68d6e2d1dd8e
Taylor Helsper <tehelsper@gmail.com>
2019-07-24 22:53:43 -0500

    Tag v0.1.25

--- rootfs/raspi4/usr/share/mynode/mynode_config_raspi.sh
+++ rootfs/raspi4/usr/share/mynode/mynode_config_raspi.sh
@@ -10,4 +10,4 @@ DEVICE_TYPE="raspi4"
 
 # Other Info
 UPGRADE_DOWNLOAD_URL="http://www.mynodebtc.com/device_api/download_latest_standard.php?type=${DEVICE_TYPE}&product_key=${PRODUCT_KEY}&serial=${SERIAL_NUM}"
-UPGRADE_DOWNLOAD_SIGNATURE_URL="http://www.mynodebtc.com/device/mynode_release_latest_raspi.sha256"
+UPGRADE_DOWNLOAD_SIGNATURE_URL="http://www.mynodebtc.com/device/hashes/mynode_release_latest_${DEVICE_TYPE}.sha256"
