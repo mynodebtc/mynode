19f8b6200506039281ecfcb671bb4f4b063a2317
Taylor Helsper <tehelsper@gmail.com>
2019-09-17 19:39:01 -0500

    Move changelog to root folder

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -9,17 +9,20 @@ mkdir -p out/rootfs_raspi4/
 
 ### Make rock64 rootfs ###
 cp -rf rootfs/standard/* out/rootfs_rock64/
+cp -f CHANGELOG out/rootfs_rock64/usr/share/mynode/changelog
 rm -f out/mynode_rootfs_rock64.tar.gz
 tar -zcvf out/mynode_rootfs_rock64.tar.gz ./out/rootfs_rock64/*
 
 ### Make raspi3 rootfs ###
 cp -rf rootfs/standard/* out/rootfs_raspi3/
 cp -rf rootfs/raspi3/* out/rootfs_raspi3/
+cp -f CHANGELOG out/rootfs_raspi3/usr/share/mynode/changelog
 rm -f out/mynode_rootfs_raspi3.tar.gz
 tar -zcvf out/mynode_rootfs_raspi3.tar.gz ./out/rootfs_raspi3/*
 
 ### Make raspi4 rootfs ###
 cp -rf rootfs/standard/* out/rootfs_raspi4/
 cp -rf rootfs/raspi4/* out/rootfs_raspi4/
+cp -f CHANGELOG out/rootfs_raspi4/usr/share/mynode/changelog
 rm -f out/mynode_rootfs_raspi4.tar.gz
 tar -zcvf out/mynode_rootfs_raspi4.tar.gz ./out/rootfs_raspi4/*
