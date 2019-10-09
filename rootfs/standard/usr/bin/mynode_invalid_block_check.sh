ec2f17ab04c6453713a01b7b9a4aa8b9e5993ee5
Taylor Helsper <tehelsper@gmail.com>
2019-10-09 18:53:23 -0500

    Tweak invalid block check timing / settings

--- rootfs/standard/usr/bin/mynode_invalid_block_check.sh
+++ rootfs/standard/usr/bin/mynode_invalid_block_check.sh
@@ -2,18 +2,24 @@
 
 sleep 5m
 
+last_invalid_block=""
 while [ 1 ]; do
     if [ -f /mnt/hdd/mynode/bitcoin/debug.log ]; then
         log=$(tail -n 10 /mnt/hdd/mynode/bitcoin/debug.log | grep "ERROR: AcceptBlockHeader")
         if [ $? == 0 ]; then
             block=$(tail -n 10 /mnt/hdd/mynode/bitcoin/debug.log | grep "ERROR: AcceptBlockHeader" | tail -n 1 | egrep -o "block [0-9a-f]+" | awk '{print $2}')
-            echo "INVALID BLOCK FOUND: $block"
-            echo "Fixing..."
-            bitcoin-cli -rpccookiefile=/mnt/hdd/mynode/bitcoin/.cookie invalidateblock $block
-            bitcoin-cli -rpccookiefile=/mnt/hdd/mynode/bitcoin/.cookie reconsiderblock $block
-            echo "Done fixing block $block"
-            sleep 30m
+            if [ "$block" = "$last_invalid_block" ]; then
+                echo "ALREADY FIXED INVALID BLOCK: $block"
+            else
+                echo "INVALID BLOCK FOUND: $block"
+                echo "Fixing..."
+                bitcoin-cli -rpccookiefile=/mnt/hdd/mynode/bitcoin/.cookie invalidateblock $block
+                bitcoin-cli -rpccookiefile=/mnt/hdd/mynode/bitcoin/.cookie reconsiderblock $block
+                last_invalid_block=$block
+                echo "Done fixing block $block"
+                sleep 5m
+            fi
         fi
     fi
-    sleep 5m
+    sleep 3m
 done
