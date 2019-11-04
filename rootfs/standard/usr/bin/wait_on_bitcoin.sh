4798821abe9980d354e43ebe3cd6355705cb0cdc
mynode-dev <dev@mynodebtc.com>
2019-11-03 21:59:43 -0600

    Speed up wait on BTC script

--- rootfs/standard/usr/bin/wait_on_bitcoin.sh
+++ rootfs/standard/usr/bin/wait_on_bitcoin.sh
@@ -6,8 +6,8 @@ set -e
 # Wait to see if bitcoind is synced
 echo "Checking if Bitcoin is synced..."
 while [ ! -f "/mnt/hdd/mynode/.mynode_bitcoind_synced" ]; do
-    echo "Bitcoin not synced, sleeping for 60 seconds..."
-    /bin/sleep 60s
+    echo "BTC not synced, sleeping 30s"
+    /bin/sleep 30s
 done
 
 # And finally, make sure bitcoind responds to API requests
