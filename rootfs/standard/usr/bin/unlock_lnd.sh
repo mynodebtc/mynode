62e86c89177e99d00b43aed4be574cfaef39ad0c
mynode-dev <dev@mynodebtc.com>
2019-11-04 20:22:46 -0600

    Fix unlock script to hold off and avoid contention if new wallet is created

--- rootfs/standard/usr/bin/unlock_lnd.sh
+++ rootfs/standard/usr/bin/unlock_lnd.sh
@@ -9,6 +9,15 @@ while true; do
         sleep 30s
     done
 
+    while [ ! -f "$LND_ADMIN_MACAROON_FILE" ]; do
+        echo "Waiting for LND admin macaroon file to exist..."
+        sleep 30s
+    done
+
+    # Sleep 15 seconds to let LND startup and avoid LN race condition
+    # See https://github.com/lightningnetwork/lnd/issues/3631
+    /bin/sleep 15s
+
     echo "Unlocking wallet..."
     /usr/bin/expect /usr/bin/unlock_lnd.tcl
     if [ $? -eq 0 ]; then
