ab14f6caba7be975c9cf92ccd342d97142152e7e
mynode-dev <dev@mynodebtc.com>
2019-11-25 19:25:15 -0600

    Tag v0.1.69

--- make_rootfs_auto.sh
+++ make_rootfs_auto.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -rf $(dirname $0)/out/rootfs_*
+
 fswatch $(dirname $0)/rootfs $(dirname $0)/CHANGELOG | (while read; do $(dirname $0)/make_rootfs.sh; echo "Update rootfs!"; done)
 
