3a91e0b8d92aac473ea88f278de454764839c8ae
BTCxZelko <34781573+BTCxZelko@users.noreply.github.com>
2019-12-03 21:36:46 -0800

    Update mynode_post_upgrade.sh

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -53,7 +53,6 @@ if [ "$CURRENT" != "$WHIRLPOOL_UPGRADE_URL" ]; then
             sudo rm -rf *.jar
         fi
         sudo -u bitcoin wget -O whirlpool.jar $WHIRLPOOL_UPGRADE_URL
-#        sudo -u bitcoin mv whirlpool-client-cli-0* whirlpool.jar
         
     mkdir -p /home/bitcoin/.mynode/
     chown -R bitcoin:bitcoin /home/bitcoin/.mynode/
