27252d4c1dc15c1a66fbb42d68c29d1944ba0f0c
mynode-dev <dev@mynodebtc.com>
2020-01-07 20:40:23 -0600

    Whirlpool client to 0.10.2

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -206,7 +206,7 @@ if [ $IS_PREMIUM -eq 1 ]; then
 fi
 
 # Install Whirlpool
-WHIRLPOOL_UPGRADE_URL=https://github.com/Samourai-Wallet/whirlpool-client-cli/releases/download/0.10.1/whirlpool-client-cli-0.10.1-run.jar
+WHIRLPOOL_UPGRADE_URL=https://github.com/Samourai-Wallet/whirlpool-client-cli/releases/download/0.10.2/whirlpool-client-cli-0.10.2-run.jar
 WHIRLPOOL_UPGRADE_URL_FILE=/home/bitcoin/.mynode/.whirlpool_url
 CURRENT=""
 if [ -f $WHIRLPOOL_UPGRADE_URL_FILE ]; then
