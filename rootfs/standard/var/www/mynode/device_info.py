a00f110252d49c33397f32565637ed1c611cbe4a
Taylor Helsper <tehelsper@gmail.com>
2020-01-10 20:04:31 -0600

    Refactor and add support for Disabled service status

--- rootfs/standard/var/www/mynode/device_info.py
+++ rootfs/standard/var/www/mynode/device_info.py
@@ -7,7 +7,48 @@ import subprocess
 # Globals
 local_ip = "unknown"
 
-# Functions
+
+#==================================
+# Manage Device
+#==================================
+def reboot_device():
+    os.system("sync")
+    os.system("/usr/bin/mynode_stop_critical_services.sh")
+    os.system("reboot")
+
+def shutdown_device():
+    os.system("sync")
+    os.system("/usr/bin/mynode_stop_critical_services.sh")
+    os.system("shutdown -h now")
+
+def factory_reset():
+    # Reset subsystems that have local data
+    delete_quicksync_data()
+
+    # Delete LND data
+    delete_lnd_data()
+
+    # Delete Tor data
+    reset_tor()
+
+    # Disable services
+    os.system("systemctl disable electrs --no-pager")
+    os.system("systemctl disable lndhub --no-pager")
+    os.system("systemctl disable btc_rpc_explorer --no-pager")
+    os.system("systemctl disable vpn --no-pager")
+
+    # Trigger drive to be reformatted on reboot
+    os.system("rm -f /mnt/hdd/.mynode")
+
+    # Reset password
+    os.system("/usr/bin/mynode_chpasswd.sh bolt")
+
+    # Reboot
+    reboot_device()
+
+#==================================
+# Manage Versions and Upgrades
+#==================================
 def get_current_version():
     current_version = "0.0"
     try:
@@ -34,41 +75,30 @@ def get_latest_version():
         latest_version = get_current_version()
     return latest_version
 
+def upgrade_device():
+    # Upgrade
+    os.system("mkdir -p /home/admin/upgrade_logs")
+    cmd = "/usr/bin/mynode_upgrade.sh > /home/admin/upgrade_logs/upgrade_log_from_{}_upgrade.txt 2>&1".format(get_current_version())
+    subprocess.call(cmd, shell=True)
+    
+    # Sync
+    os.system("sync")
+    time.sleep(1)
 
-def get_service_status_code(service_name):
-    code = os.system("systemctl status {} --no-pager".format(service_name))
-    return code
-
-def get_service_status_basic_text(service_name):
-    code = os.system("systemctl status {} --no-pager".format(service_name))
-    if code == 0:
-        return "Running"
-    return "Error"
-
-def get_service_status_color(service_name):
-    code = os.system("systemctl status {} --no-pager".format(service_name))
-    if code == 0:
-        return "green"
-    return "red"
-
-
-def get_journalctl_log(service_name):
-    try:
-        log = subprocess.check_output("journalctl -r --unit={} --no-pager | tail -n 200".format(service_name), shell=True).decode("utf8")
-    except:
-        log = "ERROR"
-    return log
-
+    # Reboot
+    reboot_device()
 
 def did_upgrade_fail():
     return os.path.isfile("/mnt/hdd/mynode/settings/upgrade_error")
 
 
+#==================================
+# Device Info
+#==================================
 def get_system_uptime():
     uptime = subprocess.check_output('awk \'{print int($1/86400)" days "int($1%86400/3600)" hour(s) "int(($1%3600)/60)" minute(s) "int($1%60)" seconds(s)"}\' /proc/uptime', shell=True)
     uptime = uptime.strip()
     return uptime
-
 
 def get_device_serial():
     serial = subprocess.check_output("cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2", shell=True)
@@ -79,12 +109,76 @@ def get_device_serial():
         serial = serial.strip()
     return serial
 
-
 def get_device_type():
     device = subprocess.check_output("mynode-get-device-type", shell=True)
     return device
 
+def get_local_ip():
+    local_ip = "unknown"
+    try:
+        local_ip = subprocess.check_output('/usr/bin/get_local_ip.py', shell=True).strip()
+    except:
+        local_ip = "error"
 
+    return local_ip
+
+def get_device_changelog():
+    changelog = ""
+    try:
+        changelog = subprocess.check_output(["cat", "/usr/share/mynode/changelog"])
+    except:
+        changelog = "ERROR"
+    return changelog
+
+def has_changed_password():
+    return os.path.isfile("/home/bitcoin/.mynode/.hashedpw")
+
+
+#==================================
+# Service Status, Enabled, Logs, etc...
+#==================================
+def is_service_enabled(service_name):
+    cmd = "systemctl is-enabled {}".format(service_name)
+    try:
+        subprocess.check_call(cmd, shell=True)
+        return True
+    except:
+        return False
+    return False
+
+def get_service_status_code(service_name):
+    code = os.system("systemctl status {} --no-pager".format(service_name))
+    return code
+
+def get_service_status_basic_text(service_name):
+    if not is_service_enabled(service_name):
+        return "Disabled"
+
+    code = os.system("systemctl status {} --no-pager".format(service_name))
+    if code == 0:
+        return "Running"
+    return "Error"
+
+def get_service_status_color(service_name):
+    if not is_service_enabled(service_name):
+        return "gray"
+
+    code = os.system("systemctl status {} --no-pager".format(service_name))
+    if code == 0:
+        return "green"
+    return "red"
+
+def get_journalctl_log(service_name):
+    try:
+        log = subprocess.check_output("journalctl -r --unit={} --no-pager | tail -n 200".format(service_name), shell=True).decode("utf8")
+    except:
+        log = "ERROR"
+    return log
+
+
+#==================================
+# Uploader Functions
+#==================================
 def is_uploader():
     return os.path.isfile("/home/bitcoin/.mynode/uploader") or \
            os.path.isfile("/mnt/hdd/mynode/settings/uploader")
@@ -96,6 +190,9 @@ def unset_uploader():
     os.system("rm -rf /mnt/hdd/mynode/settings/uploader")
 
 
+#==================================
+# QuickSync Functions
+#==================================
 def is_quicksync_enabled():
     return not os.path.isfile("/mnt/hdd/mynode/settings/quicksync_disabled")
 def disable_quicksync():
@@ -103,7 +200,41 @@ def disable_quicksync():
 def enable_quicksync():
     os.system("rm -rf /mnt/hdd/mynode/settings/quicksync_disabled")
 
+def settings_disable_quicksync():
+    stop_bitcoind()
+    stop_quicksync()
+    disable_quicksync()
+    delete_quicksync_data()
+    reboot_device()
 
+def settings_enable_quicksync():
+    stop_bitcoind()
+    stop_quicksync()
+    enable_quicksync()
+    delete_quicksync_data()
+    reboot_device()
+
+def delete_quicksync_data():
+    os.system("rm -rf /mnt/hdd/mynode/quicksync")
+    os.system("rm -rf /home/bitcoin/.config/transmission") # Old dir
+    os.system("rm -rf /mnt/hdd/mynode/.config/transmission")
+
+def stop_quicksync():
+    os.system("systemctl stop quicksync")
+
+def restart_quicksync():
+    os.system('echo "quicksync_reset" > /mnt/hdd/mynode/.mynode_status')
+    stop_bitcoind()
+    stop_quicksync()
+    delete_bitcoin_data()
+    delete_quicksync_data()
+    enable_quicksync()
+    reboot_device()
+
+
+#==================================
+# Product Key Functions
+#==================================
 def set_skipped_product_key():
     os.system("touch /home/bitcoin/.mynode/.product_key_skipped")
     os.system("touch /mnt/hdd/mynode/settings/.product_key_skipped")
@@ -146,6 +277,9 @@ def delete_product_key_error():
     os.system("rm -rf /mnt/hdd/mynode/settings/.product_key_error")
 
 
+#==================================
+# Drive Repair Functions
+#==================================
 def is_drive_being_repaired():
     return os.path.isfile("/tmp/repairing_drive")
 def has_fsck_error():
@@ -159,171 +293,12 @@ def get_fsck_results():
     return "ERROR"
 
 
+#==================================
+# Docker Functions
+#==================================
 def is_installing_docker_images():
     return os.path.isfile("/tmp/installing_docker_images")
 
-
-def get_local_ip():
-    local_ip = "unknown"
-    try:
-        local_ip = subprocess.check_output('/usr/bin/get_local_ip.py', shell=True).strip()
-    except:
-        local_ip = "error"
-
-    return local_ip
-
-
-def get_device_changelog():
-    changelog = ""
-    try:
-        changelog = subprocess.check_output(["cat", "/usr/share/mynode/changelog"])
-    except:
-        changelog = "ERROR"
-    return changelog
-
-def has_changed_password():
-    return os.path.isfile("/home/bitcoin/.mynode/.hashedpw")
-
-def get_bitcoin_rpc_password():
-    try:
-        with open("/mnt/hdd/mynode/settings/.btcrpcpw", "r") as f:
-            return f.read()
-    except:
-        return "ERROR"
-    return "ERROR"
-
-def stop_bitcoind():
-    os.system("systemctl stop bitcoind")
-
-def stop_lnd():
-    os.system("systemctl stop lnd")
-
-def restart_lnd():
-    os.system("systemctl restart lnd")
-
-def stop_quicksync():
-    os.system("systemctl stop quicksync")
-
-
-def settings_disable_quicksync():
-    stop_bitcoind()
-    stop_quicksync()
-    disable_quicksync()
-    delete_quicksync_data()
-    reboot_device()
-
-def settings_enable_quicksync():
-    stop_bitcoind()
-    stop_quicksync()
-    enable_quicksync()
-    delete_quicksync_data()
-    reboot_device()
-
-
-def reset_bitcoin_env_file():
-    os.system("echo 'BTCARGS=' > "+BITCOIN_ENV_FILE)
-
-
-def delete_bitcoin_data():
-    os.system("rm -rf /mnt/hdd/mynode/bitcoin")
-    os.system("rm -rf /mnt/hdd/mynode/quicksync/.quicksync_complete")
-    os.system("rm -rf /mnt/hdd/mynode/settings/.btcrpc_environment")
-    os.system("rm -rf /mnt/hdd/mynode/settings/.btcrpcpw")
-
-
-def delete_quicksync_data():
-    os.system("rm -rf /mnt/hdd/mynode/quicksync")
-    os.system("rm -rf /home/bitcoin/.config/transmission") # Old dir
-    os.system("rm -rf /mnt/hdd/mynode/.config/transmission")
-
-
-def delete_lnd_data():
-    #os.system("rm -f "+LND_WALLET_FILE)
-    os.system("rm -rf "+LND_DATA_FOLDER)
-    os.system("rm -rf /home/bitcoin/.lnd-admin/credentials.json")
-    os.system("rm -rf /mnt/hdd/mynode/settings/.lndpw")
-    os.system("rm -rf /home/admin/.lnd/")
-    return True
-
-
-def reboot_device():
-    os.system("sync")
-    os.system("/usr/bin/mynode_stop_critical_services.sh")
-    os.system("reboot")
-
-
-def shutdown_device():
-    os.system("sync")
-    os.system("/usr/bin/mynode_stop_critical_services.sh")
-    os.system("shutdown -h now")
-
-
-def reset_blockchain():
-    stop_bitcoind()
-    delete_bitcoin_data()
-    reboot_device()
-
-
-def restart_quicksync():
-    os.system('echo "quicksync_reset" > /mnt/hdd/mynode/.mynode_status')
-    stop_bitcoind()
-    stop_quicksync()
-    delete_bitcoin_data()
-    delete_quicksync_data()
-    enable_quicksync()
-    reboot_device()
-
-
-def reset_tor():
-    os.system("rm -rf /var/lib/tor/*")
-    os.system("rm -rf /mnt/hdd/mynode/bitcoin/onion_private_key")
-    os.system("rm -rf /mnt/hdd/mynode/lnd/v2_onion_private_key")
-
-
-def factory_reset():
-    # Reset subsystems that have local data
-    delete_quicksync_data()
-
-    # Delete LND data
-    delete_lnd_data()
-
-    # Delete Tor data
-    reset_tor()
-
-    # Disable services
-    os.system("systemctl disable electrs --no-pager")
-    os.system("systemctl disable lndhub --no-pager")
-    os.system("systemctl disable btc_rpc_explorer --no-pager")
-    os.system("systemctl disable vpn --no-pager")
-
-    # Trigger drive to be reformatted on reboot
-    os.system("rm -f /mnt/hdd/.mynode")
-
-    # Reset password
-    os.system("/usr/bin/mynode_chpasswd.sh bolt")
-
-    # Reboot
-    reboot_device()
-
-
-def upgrade_device():
-    # Upgrade
-    os.system("mkdir -p /home/admin/upgrade_logs")
-    cmd = "/usr/bin/mynode_upgrade.sh > /home/admin/upgrade_logs/upgrade_log_from_{}_upgrade.txt 2>&1".format(get_current_version())
-    subprocess.call(cmd, shell=True)
-    
-    # Sync
-    os.system("sync")
-    time.sleep(1)
-
-    # Reboot
-    reboot_device()
-
-
-
-#=========================================
-# Service Status Functions
-#=========================================
 def get_docker_image_build_status():
     status_code = get_service_status_code("docker_images")
 
@@ -342,3 +317,58 @@ def get_docker_image_build_status_color():
     if status_code != 0:
         return "red"
     return "green"
+
+#==================================
+# Bitcoin Functions
+#==================================
+def get_bitcoin_rpc_password():
+    try:
+        with open("/mnt/hdd/mynode/settings/.btcrpcpw", "r") as f:
+            return f.read()
+    except:
+        return "ERROR"
+    return "ERROR"
+
+def stop_bitcoind():
+    os.system("systemctl stop bitcoind")
+
+def reset_bitcoin_env_file():
+    os.system("echo 'BTCARGS=' > "+BITCOIN_ENV_FILE)
+
+def delete_bitcoin_data():
+    os.system("rm -rf /mnt/hdd/mynode/bitcoin")
+    os.system("rm -rf /mnt/hdd/mynode/quicksync/.quicksync_complete")
+    os.system("rm -rf /mnt/hdd/mynode/settings/.btcrpc_environment")
+    os.system("rm -rf /mnt/hdd/mynode/settings/.btcrpcpw")
+
+def reset_blockchain():
+    stop_bitcoind()
+    delete_bitcoin_data()
+    reboot_device()
+
+
+#==================================
+# LND Functions
+#==================================
+def stop_lnd():
+    os.system("systemctl stop lnd")
+
+def restart_lnd():
+    os.system("systemctl restart lnd")
+
+def delete_lnd_data():
+    #os.system("rm -f "+LND_WALLET_FILE)
+    os.system("rm -rf "+LND_DATA_FOLDER)
+    os.system("rm -rf /home/bitcoin/.lnd-admin/credentials.json")
+    os.system("rm -rf /mnt/hdd/mynode/settings/.lndpw")
+    os.system("rm -rf /home/admin/.lnd/")
+    return True
+
+
+#==================================
+# Tor Functions
+#==================================
+def reset_tor():
+    os.system("rm -rf /var/lib/tor/*")
+    os.system("rm -rf /mnt/hdd/mynode/bitcoin/onion_private_key")
+    os.system("rm -rf /mnt/hdd/mynode/lnd/v2_onion_private_key")
