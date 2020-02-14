51c5a57feb62b7da181d4b05524dd6ad6ee36a79
Amiga500 <16525337+Amiga500@users.noreply.github.com>
2020-02-14 16:00:49 +0100

    RTL v0.6.5 on mynode_post_upgrade.sh

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -295,7 +295,7 @@ if [ "$CURRENT" != "$WHIRLPOOL_UPGRADE_URL" ]; then
 fi
 
 # Upgrade RTL
-RTL_VERSION="v0.6.4"
+RTL_VERSION="v0.6.5"
 RTL_UPGRADE_URL=https://github.com/Ride-The-Lightning/RTL/archive/$RTL_VERSION.tar.gz
 RTL_UPGRADE_ASC_URL=https://github.com/Ride-The-Lightning/RTL/releases/download/$RTL_VERSION/$RTL_VERSION.tar.gz.asc
 RTL_UPGRADE_URL_FILE=/home/bitcoin/.mynode/.rtl_url
