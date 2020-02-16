f3d437f80007e4c74cb9e4cdc26dd29a3f039554
abhishandy <abhishek.sandy@gmail.com>
2020-02-16 02:01:43 -0500

    replaced ugly tar output with simple messages

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 for i in 'rock64' 'rockpro64' 'raspi3' 'raspi4' 'debian'; do
+	echo Creating root file system for $i
 	mkdir -p out/rootfs_$i/
 	rsync -r -u rootfs/standard/* out/rootfs_$i/
 	rsync -r -u rootfs/$i/* out/rootfs_$i/
 	rsync -r -u CHANGELOG out/rootfs_$i/usr/share/mynode/changelog
-    cp -f setup/setup_device.sh out/setup_device.sh
+	cp -f setup/setup_device.sh out/setup_device.sh
 
 	rm -f out/mynode_rootfs_$i.tar.gz
-	tar -zcvf out/mynode_rootfs_$i.tar.gz ./out/rootfs_$i/*
+	tar -zcf out/mynode_rootfs_$i.tar.gz out/rootfs_$i/*
 done
+echo Done!
