0875a4790fb95ac8af4d7e1281a72a9212ec2926
mynode-dev <dev@mynodebtc.com>
2020-02-17 22:21:38 -0600

    Hide status until bitcoin is syced

--- rootfs/standard/var/www/mynode/settings.py
+++ rootfs/standard/var/www/mynode/settings.py
@@ -1,6 +1,7 @@
 from config import *
 from flask import Blueprint, render_template, session, abort, Markup, request, redirect, send_from_directory, url_for, flash
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from bitcoind import is_bitcoind_synced
 from pprint import pprint, pformat
 from threading import Timer
 from thread_functions import *
@@ -151,6 +152,7 @@ def page_settings():
         "quicksync_status_log": quicksync_status_log,
         "quicksync_status": quicksync_status,
         "quicksync_status_color": quicksync_status_color,
+        "is_bitcoin_synced": is_bitcoind_synced(),
         "bitcoin_status_log": bitcoin_status_log,
         "bitcoin_status": get_service_status_basic_text("bitcoind"),
         "bitcoin_status_color": get_service_status_color("bitcoind"),
