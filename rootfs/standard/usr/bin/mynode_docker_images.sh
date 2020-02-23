db685bd342b2bb7b1035151e61d5c4cd78dcb901
mynode-dev <dev@mynodebtc.com>
2020-02-23 15:55:37 -0600

    Make docker_images a service again to get better status

--- rootfs/standard/usr/bin/mynode_docker_images.sh
+++ rootfs/standard/usr/bin/mynode_docker_images.sh
@@ -13,62 +13,66 @@ sleep 60s
 echo "Waiting on bitcoin to sync so drive usage is lower..."
 /usr/bin/wait_on_bitcoin.sh
 
+while true; do
+    echo "Checking for building new docker images..."
+    touch /tmp/installing_docker_images
 
-echo "Checking for building new docker images..."
-touch /tmp/installing_docker_images
+    # Pull images that don't need to be built
+    docker pull netdata/netdata
 
-# Pull images that don't need to be built
-docker pull netdata/netdata
+    # Upgrade WebSSH2
+    echo "Checking for new webssh2..."
+    WEBSSH2_UPGRADE_URL=https://github.com/billchurch/webssh2/archive/v0.2.10-0.tar.gz
+    WEBSSH2_UPGRADE_URL_FILE=/mnt/hdd/mynode/settings/webssh2_url
+    CURRENT=""
+    if [ -f $WEBSSH2_UPGRADE_URL_FILE ]; then
+        CURRENT=$(cat $WEBSSH2_UPGRADE_URL_FILE)
+    fi
+    if [ "$CURRENT" != "$WEBSSH2_UPGRADE_URL" ]; then
+        docker rmi webssh2 || true
 
-# Upgrade WebSSH2
-echo "Checking for new webssh2..."
-WEBSSH2_UPGRADE_URL=https://github.com/billchurch/webssh2/archive/v0.2.10-0.tar.gz
-WEBSSH2_UPGRADE_URL_FILE=/mnt/hdd/mynode/settings/webssh2_url
-CURRENT=""
-if [ -f $WEBSSH2_UPGRADE_URL_FILE ]; then
-    CURRENT=$(cat $WEBSSH2_UPGRADE_URL_FILE)
-fi
-if [ "$CURRENT" != "$WEBSSH2_UPGRADE_URL" ]; then
-    docker rmi webssh2 || true
+        cd /tmp/
+        rm -rf webssh2
+        wget $WEBSSH2_UPGRADE_URL -O webssh2.tar.gz
+        tar -xvf webssh2.tar.gz
+        rm webssh2.tar.gz
+        mv webssh2-* webssh2
+        cd webssh2
+        docker build -t webssh2 .
 
-    cd /tmp/
-    rm -rf webssh2
-    wget $WEBSSH2_UPGRADE_URL -O webssh2.tar.gz
-    tar -xvf webssh2.tar.gz
-    rm webssh2.tar.gz
-    mv webssh2-* webssh2
-    cd webssh2
-    docker build -t webssh2 .
+        echo $WEBSSH2_UPGRADE_URL > $WEBSSH2_UPGRADE_URL_FILE
+    fi
 
-    echo $WEBSSH2_UPGRADE_URL > $WEBSSH2_UPGRADE_URL_FILE
-fi
+    # Upgrade mempool.space
+    echo "Checking for new mempool.space..."
+    MEMPOOLSPACE_UPGRADE_URL=https://github.com/mempool-space/mempool.space/archive/8835c399e9b00c2579ed0bbd72f8cca4c5823dad.zip
+    MEMPOOLSPACE_UPGRADE_URL_FILE=/mnt/hdd/mynode/settings/mempoolspace_url
+    CURRENT=""
+    if [ -f $MEMPOOLSPACE_UPGRADE_URL_FILE ]; then
+        CURRENT=$(cat $MEMPOOLSPACE_UPGRADE_URL_FILE)
+    fi
+    if [ "$CURRENT" != "$MEMPOOLSPACE_UPGRADE_URL" ]; then
+        docker rmi mempoolspace || true
 
-# Upgrade mempool.space
-echo "Checking for new mempool.space..."
-MEMPOOLSPACE_UPGRADE_URL=https://github.com/mempool-space/mempool.space/archive/8835c399e9b00c2579ed0bbd72f8cca4c5823dad.zip
-MEMPOOLSPACE_UPGRADE_URL_FILE=/mnt/hdd/mynode/settings/mempoolspace_url
-CURRENT=""
-if [ -f $MEMPOOLSPACE_UPGRADE_URL_FILE ]; then
-    CURRENT=$(cat $MEMPOOLSPACE_UPGRADE_URL_FILE)
-fi
-if [ "$CURRENT" != "$MEMPOOLSPACE_UPGRADE_URL" ]; then
-    docker rmi mempoolspace || true
+        cd /opt/mynode
+        rm -rf mempoolspace
+        wget $MEMPOOLSPACE_UPGRADE_URL -O mempool.zip
+        unzip -o mempool.zip
+        rm mempool.zip
+        mv mempool* mempoolspace
+        cd mempoolspace
+        sync
+        sleep 3s
+        docker build -t mempoolspace .
 
-    cd /opt/mynode
-    rm -rf mempoolspace
-    wget $MEMPOOLSPACE_UPGRADE_URL -O mempool.zip
-    unzip -o mempool.zip
-    rm mempool.zip
-    mv mempool* mempoolspace
-    cd mempoolspace
-    sync
-    sleep 3s
-    docker build -t mempoolspace .
+        echo $MEMPOOLSPACE_UPGRADE_URL > $MEMPOOLSPACE_UPGRADE_URL_FILE
+    fi
 
-    echo $MEMPOOLSPACE_UPGRADE_URL > $MEMPOOLSPACE_UPGRADE_URL_FILE
-fi
+    rm -f /tmp/installing_docker_images
 
-rm -f /tmp/installing_docker_images
+    # Wait a day
+    sleep 1d
+done
 
-
-exit 0
+# We should not exit
+exit 1
