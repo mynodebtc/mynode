d253e56ee16288fd6ba342f01736c9ce90bb48de
Mitchell Cash <mitchell@mitchellcash.com>
2020-03-10 00:01:42 +0000

    Bump Loop to 0.5.0-beta (#243)

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -179,7 +179,7 @@ fi
 
 # Upgrade Loopd
 echo "Upgrading loopd..."
-LOOP_VERSION="v0.4.0-beta"
+LOOP_VERSION="v0.5.0-beta"
 LOOP_ARCH="loop-linux-armv7"
 if [ $IS_X86 = 1 ]; then
     LOOP_ARCH="loop-linux-amd64"
