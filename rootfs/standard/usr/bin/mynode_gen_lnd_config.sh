62258ef87822791a0f8871c5b83fe3d2025b37f6
mynode-dev <dev@mynodebtc.com>
2020-04-05 17:43:06 -0500

    Explicitly add onion url to LND TLS cert

--- rootfs/standard/usr/bin/mynode_gen_lnd_config.sh
+++ rootfs/standard/usr/bin/mynode_gen_lnd_config.sh
@@ -16,6 +16,15 @@ else
     fi
 fi
 
+# Append tor domain
+if [ -f /var/lib/tor/mynode_lnd/hostname ]; then
+    echo "" >> /mnt/hdd/mynode/lnd/lnd.conf
+    echo "[Application Options]" >> /mnt/hdd/mynode/lnd/lnd.conf
+    ONION_URL=$(cat /var/lib/tor/mynode_lnd/hostname)
+    echo "tlsextradomain=$ONION_URL" >> /mnt/hdd/mynode/lnd/lnd.conf
+    echo "" >> /mnt/hdd/mynode/lnd/lnd.conf
+fi
+
 ALIAS=$(cat /mnt/hdd/mynode/settings/.lndalias)
 sed -i "s/alias=.*/alias=$ALIAS/g" /mnt/hdd/mynode/lnd/lnd.conf
 chown bitcoin:bitcoin /mnt/hdd/mynode/lnd/lnd.conf
