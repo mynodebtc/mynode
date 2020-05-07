e5042070e6e79812b2fda1e9b68da5f8fa96544c
mynode-dev <dev@mynodebtc.com>
2020-05-07 17:38:03 -0500

    Prevent some logs from growing forever

--- rootfs/standard/usr/bin/mynode_logrotate.sh
+++ rootfs/standard/usr/bin/mynode_logrotate.sh
@@ -4,7 +4,18 @@ set -e
 set -x
 
 while true; do
+    # Rotate logs
     logrotate /etc/logrotate.conf
+    
+    # Check for any "lost" logs that are growing too large
+    LARGE_FILE_COUNT=$(find /var/log/ -type f -size +10M | wc -l)
+    if [ "$LARGE_FILE_COUNT" -gt "0" ]; then
+        # Delete the files and restart syslog
+        find /var/log/ -type f -size +10M | sudo xargs rm -f
+        systemctl restart syslog
+    fi
+
+    # Sleep
     sleep 10m
 done
 
