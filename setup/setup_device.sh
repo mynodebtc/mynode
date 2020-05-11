947aa84914fd51be3fb48fe23e59775a18bddf67
Amiga500 <16525337+Amiga500@users.noreply.github.com>
2020-05-11 18:39:46 +0200

    RTL v0.7.1

--- setup/setup_device.sh
+++ setup/setup_device.sh
@@ -438,7 +438,7 @@ fi
 
 
 # Install RTL
-RTL_VERSION="v0.7.0"
+RTL_VERSION="v0.7.1"
 RTL_UPGRADE_URL=https://github.com/Ride-The-Lightning/RTL/archive/$RTL_VERSION.tar.gz
 RTL_UPGRADE_ASC_URL=https://github.com/Ride-The-Lightning/RTL/releases/download/$RTL_VERSION/$RTL_VERSION.tar.gz.asc
 RTL_UPGRADE_URL_FILE=/home/bitcoin/.mynode/.rtl_url
