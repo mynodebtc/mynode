13e364cfc7b22ba5e3d949d014cb94452c56d925
Amiga500 <16525337+Amiga500@users.noreply.github.com>
2020-06-06 10:08:19 +0200

    Update mynode_post_upgrade.sh

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -401,7 +401,7 @@ if [ "$CURRENT" != "$RTL_UPGRADE_URL" ]; then
 fi
 
 # Upgrade BTC RPC Explorer
-BTCRPCEXPLORER_UPGRADE_URL=https://github.com/janoside/btc-rpc-explorer/archive/v2.0.0.tar.gz
+BTCRPCEXPLORER_UPGRADE_URL=https://github.com/janoside/btc-rpc-explorer/archive/v2.0.1.tar.gz
 BTCRPCEXPLORER_UPGRADE_URL_FILE=/home/bitcoin/.mynode/.btcrpcexplorer_url
 CURRENT=""
 if [ -f $BTCRPCEXPLORER_UPGRADE_URL_FILE ]; then
