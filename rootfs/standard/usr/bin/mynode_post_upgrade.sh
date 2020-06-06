5797f1ff129ee2408849802aec1aaaa4d366f55c
Amiga500 <16525337+Amiga500@users.noreply.github.com>
2020-06-06 10:03:21 +0200

    Update mynode_post_upgrade.sh

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -147,7 +147,7 @@ fi
 
 # Upgrade LND
 echo "Upgrading LND..."
-LND_VERSION="v0.10.0-beta"
+LND_VERSION="v0.10.1-beta"
 LND_ARCH="lnd-linux-armv7"
 if [ $IS_X86 = 1 ]; then
     LND_ARCH="lnd-linux-amd64"
