c524b043388ef4bb0cb923e04fd3bbacae5ca809
Amiga500 <16525337+Amiga500@users.noreply.github.com>
2020-06-10 03:46:49 +0200

    LND v0.10.1 + BTC RPC Explorer v2.0.1 (#330)
    * Update setup_device.sh
    
    * Update mynode_post_upgrade.sh
    
    * Update setup_device.sh
    
    * Update mynode_post_upgrade.sh
    
    * Update mynode_post_upgrade.sh

--- setup/setup_device.sh
+++ setup/setup_device.sh
@@ -262,7 +262,7 @@ fi
 cd ~
 
 # Install Lightning
-LND_VERSION="v0.10.0-beta"
+LND_VERSION="v0.10.1-beta"
 LND_ARCH="lnd-linux-armv7"
 if [ $IS_X86 = 1 ]; then
     LND_ARCH="lnd-linux-amd64"
@@ -519,7 +519,7 @@ fi
 
 
 # Install BTC RPC Explorer
-BTCRPCEXPLORER_UPGRADE_URL=https://github.com/janoside/btc-rpc-explorer/archive/v2.0.0.tar.gz
+BTCRPCEXPLORER_UPGRADE_URL=https://github.com/janoside/btc-rpc-explorer/archive/v2.0.1.tar.gz
 BTCRPCEXPLORER_UPGRADE_URL_FILE=/home/bitcoin/.mynode/.btcrpcexplorer_url
 CURRENT=""
 if [ -f $BTCRPCEXPLORER_UPGRADE_URL_FILE ]; then
