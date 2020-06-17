15a0091af168b974b1668adb2529065547778e46
mynode-dev <dev@mynodebtc.com>
2020-06-16 23:15:53 -0500

    Use local IP address for Caravan suggestion

--- rootfs/standard/var/www/mynode/caravan.py
+++ rootfs/standard/var/www/mynode/caravan.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, render_template, redirect
 from settings import read_ui_settings
 from user_management import check_logged_in
+from device_info import *
 import subprocess
 import re
 import os
@@ -17,6 +18,7 @@ def caravan_page():
     # Load page
     templateData = {
         "title": "myNode Caravan",
+        "local_ip": get_local_ip(),
         "ui_settings": read_ui_settings()
     }
     return render_template('caravan.html', **templateData)
