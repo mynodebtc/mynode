f3117f56856091f1b389a174752cc1982ef19e22
mynode-dev <dev@mynodebtc.com>
2020-07-29 19:19:55 -0500

    Fix issues with setup_device.sh; Refactoring

--- rootfs/standard/usr/bin/mynode_startup.sh
+++ rootfs/standard/usr/bin/mynode_startup.sh
@@ -277,12 +277,20 @@ if [ ! -L /home/bitcoin/.specter ]; then
 fi
 
 # Setup Thunderhub
+mkdir -p /mnt/hdd/mynode/thunderhub/
+if [ ! -f /mnt/hdd/mynode/thunderhub/.env.local ]; then
+    cp -f /usr/share/mynode/thunderhub.env /mnt/hdd/mynode/thunderhub/.env.local
+fi
+if [ ! -f /mnt/hdd/mynode/thunderhub/thub_config.yaml ]; then
+    cp -f /usr/share/mynode/thub_config.yaml /mnt/hdd/mynode/thunderhub/thub_config.yaml
+fi
 if [ -f /mnt/hdd/mynode/thunderhub/thub_config.yaml ]; then
     if [ -f /home/bitcoin/.mynode/.hashedpw_bcrypt ]; then
         HASH_BCRYPT=$(cat /home/bitcoin/.mynode/.hashedpw_bcrypt)
         sed -i "s#masterPassword:.*#masterPassword: \"thunderhub-$HASH_BCRYPT\"#g" /mnt/hdd/mynode/thunderhub/thub_config.yaml
     fi
 fi
+chown -R bitcoin:bitcoin /mnt/hdd/mynode/thunderhub
 
 # Setup udev
 chown root:root /etc/udev/rules.d/* || true
