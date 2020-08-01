a93bf3c01a719358d4f9364de58c2d617d3461dc
Taylor Helsper <tehelsper@gmail.com>
2020-07-31 22:51:59 -0500

    Randomize flask secret key

--- rootfs/standard/var/www/mynode/mynode.py
+++ rootfs/standard/var/www/mynode/mynode.py
@@ -876,7 +876,8 @@ def before_first_request():
 
     app.register_error_handler(LoginError, handle_login_exception)
 
-    app.secret_key = 'NoZlPx7t15foPfKpivbVrTrTy2bTQ99chJoz3LFmf5BFsh3Nz4ud0mMpGjtB4bhP'
+    app.config["SESSION_COOKIE_NAME"] = "mynode_session_id"
+    app.secret_key = get_flask_secret_key()
     app.permanent_session_lifetime = timedelta(days=90)
 
     app.logger.info("BEFORE_FIRST_REQUEST")
