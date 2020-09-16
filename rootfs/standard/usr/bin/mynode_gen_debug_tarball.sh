af6fafead094a73373f568bf423b7215c5848f05
Taylor Helsper <tehelsper@gmail.com>
2020-09-15 21:55:25 -0500

    Skip docker ps if drive not mounted to prevent hang

--- rootfs/standard/usr/bin/mynode_gen_debug_tarball.sh
+++ rootfs/standard/usr/bin/mynode_gen_debug_tarball.sh
@@ -14,14 +14,22 @@ cp -rf /home/admin/upgrade_logs /tmp/mynode_info/
 cp /mnt/hdd/mynode/bitcoin/debug.log /tmp/mynode_info/bitcoin_debug.log
 
 echo "" > /tmp/mynode_info/device_info
+
 echo "##### df -h #####" >> /tmp/mynode_info/device_info
 df -h >> /tmp/mynode_info/device_info
 echo "" >> /tmp/mynode_info/device_info
+
 echo "##### mount #####" >> /tmp/mynode_info/device_info
 mount >> /tmp/mynode_info/device_info
 echo "" >> /tmp/mynode_info/device_info
+
 echo "##### docker ps #####" >> /tmp/mynode_info/device_info
-docker ps >> /tmp/mynode_info/device_info
+if grep -qs '/mnt/hdd' /proc/mounts; then
+    docker ps >> /tmp/mynode_info/device_info
+else
+    echo "Drive not mounted - skipping 'docker ps'" >> /tmp/mynode_info/device_info
+fi
+echo "" >> /tmp/mynode_info/device_info
 
 # Create tarball
 tar -czvf /tmp/mynode_logs.tar.gz /var/log/ /tmp/mynode_info/
