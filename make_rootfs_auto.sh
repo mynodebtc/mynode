2236b71f0efb8f8ba8eb8dd9dc80caf3f0e8c241
Taylor Helsper <taylor.helsper@adtran.com>
2020-10-29 17:22:15 -0500

    Update script for rootfs to work better on Debian

--- make_rootfs_auto.sh
+++ make_rootfs_auto.sh
@@ -2,5 +2,7 @@
 
 rm -rf $(dirname $0)/out/rootfs_*
 
-fswatch $(dirname $0)/rootfs $(dirname $0)/CHANGELOG | (while read; do $(dirname $0)/make_rootfs.sh; echo "Update rootfs!"; done)
+# Did not work well on Linux.... works on OSX :-/
+#fswatch -o $(dirname $0)/rootfs $(dirname $0)/CHANGELOG | (while read; do $(dirname $0)/make_rootfs.sh; echo "Update rootfs!"; done)
 
+while true; do find $(dirname $0)/rootfs/ | entr -d -s $(dirname $0)/make_rootfs.sh; done
