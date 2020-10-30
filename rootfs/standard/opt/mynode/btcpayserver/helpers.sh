f5f5d873d9ec9bb334c9ab58504c736d0940c18d
mynode-dev <dev@mynodebtc.com>
2020-10-29 22:19:48 -0500

    Add re-install option for BTC Pay Server

--- rootfs/standard/opt/mynode/btcpayserver/helpers.sh
+++ rootfs/standard/opt/mynode/btcpayserver/helpers.sh
@@ -37,3 +37,14 @@ btcpay_restart() {
     fi
     popd > /dev/null
 }
+
+btcpay_remove() {
+    pushd . > /dev/null
+    cd "$(dirname "$BTCPAY_ENV_FILE")"
+    docker-compose -f $BTCPAY_DOCKER_COMPOSE down -v -t "${COMPOSE_HTTP_TIMEOUT:-180}"
+    # Depending on docker-compose, the timeout does not work.
+    if ! [ $? -eq 0 ]; then
+        docker-compose -f $BTCPAY_DOCKER_COMPOSE -v down
+    fi
+    popd > /dev/null
+}
