d71b7ec091a416ab929b01d56bdfb975558cbc9b
mynode-dev <dev@mynodebtc.com>
2020-11-08 15:54:54 -0600

    Upgrade loop to v0.11.0

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -8,9 +8,7 @@ LND_VERSION="v0.11.1-beta"
 LND_VERSION_FILE=/home/bitcoin/.mynode/lnd_version
 LND_LATEST_VERSION_FILE=/home/bitcoin/.mynode/lnd_version_latest
 
-# Upgrade to v0.9.0 once RTL or other web wallets support it
-# https://github.com/Ride-The-Lightning/RTL/issues/472
-LOOP_VERSION="v0.8.1-beta" 
+LOOP_VERSION="v0.11.0-beta" 
 LOOP_VERSION_FILE=/home/bitcoin/.mynode/loop_version
 LOOP_LATEST_VERSION_FILE=/home/bitcoin/.mynode/loop_version_latest
 
