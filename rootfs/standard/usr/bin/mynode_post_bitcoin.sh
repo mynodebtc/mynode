537652f0551bfbdd8b4368588c114ce104aa4e88
Taylor Helsper <tehelsper@gmail.com>
2021-03-08 22:37:46 -0600

    Admin bitcoin-cli works in testnet mode now

--- rootfs/standard/usr/bin/mynode_post_bitcoin.sh
+++ rootfs/standard/usr/bin/mynode_post_bitcoin.sh
@@ -10,6 +10,12 @@ sleep 60s
 cp -f /mnt/hdd/mynode/bitcoin/.cookie /home/admin/.bitcoin/.cookie
 chown admin:admin /home/admin/.bitcoin/.cookie
 
+if [ -f /mnt/hdd/mynode/bitcoin/testnet3/.cookie ]; then
+    mkdir -p /mnt/hdd/mynode/bitcoin/testnet3
+    cp -f /mnt/hdd/mynode/bitcoin/testnet3/.cookie /home/admin/.bitcoin/testnet3/.cookie
+    chown -R admin:admin /home/admin/.bitcoin/testnet3
+fi
+
 # Make default wallets
 bitcoin-cli createwallet joinmarket_wallet.dat > /dev/null  2>&1 || true
 bitcoin-cli loadwallet joinmarket_wallet.dat > /dev/null  2>&1 || true
