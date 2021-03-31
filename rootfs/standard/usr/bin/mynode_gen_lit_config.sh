afb55164452088487aaf2f7cbf8c90a2509d2060
Taylor Helsper <tehelsper@gmail.com>
2021-03-30 22:08:35 -0500

    Simplify config

--- rootfs/standard/usr/bin/mynode_gen_lit_config.sh
+++ rootfs/standard/usr/bin/mynode_gen_lit_config.sh
@@ -13,27 +13,11 @@ else
     # Generate a default config
     cp -f /usr/share/mynode/lit.conf /mnt/hdd/mynode/lit/lit.conf
 
-    # Append other sections
-    if [ -f /mnt/hdd/mynode/settings/.btc_lnd_tor_enabled ]; then
-        cat /usr/share/mynode/lit_tor.conf >> /mnt/hdd/mynode/lit/lit.conf
-    else
-        cat /usr/share/mynode/lit_ipv4.conf >> /mnt/hdd/mynode/lit/lit.conf
-    fi
-
-    # Append Mainnet/Testnet section
+    # Adjust Mainnet/Testnet
     if [ -f /mnt/hdd/mynode/settings/.testnet_enabled ]; then
         sed -i "s/mainnet/testnet/g" /mnt/hdd/mynode/lit/lit.conf
     fi
 fi
 
-# Append tor domain
-if [ -f /var/lib/tor/mynode_lnd/hostname ]; then
-    echo "" >> /mnt/hdd/mynode/lit/lit.conf
-    ONION_URL=$(cat /var/lib/tor/mynode_lnd/hostname)
-    echo "lnd.tlsextradomain=$ONION_URL" >> /mnt/hdd/mynode/lit/lit.conf
-    echo "" >> /mnt/hdd/mynode/lit/lit.conf
-fi
 
-ALIAS=$(cat /mnt/hdd/mynode/settings/.lndalias)
-sed -i "s/lnd.alias=.*/lnd.alias=$ALIAS/g" /mnt/hdd/mynode/lit/lit.conf
 chown bitcoin:bitcoin /mnt/hdd/mynode/lit/lit.conf
