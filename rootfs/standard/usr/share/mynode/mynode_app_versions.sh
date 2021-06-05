f834a0bd2e9467d69c42e4076ae2111e2c1b5424
mynode-dev <dev@mynodebtc.com>
2021-06-04 19:50:28 -0500

    Upgrade Sphinx Relay to v2.1.3

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -109,7 +109,7 @@ BOS_VERSION="9.2.0"
 BOS_VERSION_FILE=/home/bitcoin/.mynode/bos_version
 BOS_LATEST_VERSION_FILE=/home/bitcoin/.mynode/bos_version_latest
 
-SPHINXRELAY_VERSION="v2.0.14"
+SPHINXRELAY_VERSION="v2.1.3"
 SPHINXRELAY_VERSION_FILE=/home/bitcoin/.mynode/sphinxrelay_version
 SPHINXRELAY_LATEST_VERSION_FILE=/home/bitcoin/.mynode/sphinxrelay_version_latest
 
