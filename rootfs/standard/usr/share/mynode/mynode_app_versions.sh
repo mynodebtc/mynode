0384fbf51008b4baecf0b3eb2def8358d4c8f0b0
mynode-dev <dev@mynodebtc.com>
2022-01-27 23:06:57 -0600

    Upgrade Mempool to v2.3.1

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -32,7 +32,7 @@ ELECTRS_VERSION="v0.9.3"
 ELECTRS_VERSION_FILE=/home/bitcoin/.mynode/electrs_version
 ELECTRS_LATEST_VERSION_FILE=/home/bitcoin/.mynode/electrs_version_latest
 
-MEMPOOL_VERSION="v2.3.0"
+MEMPOOL_VERSION="v2.3.1"
 MEMPOOL_VERSION_FILE=/mnt/hdd/mynode/settings/mempool_version
 MEMPOOL_LATEST_VERSION_FILE=/mnt/hdd/mynode/settings/mempool_version_latest
 
