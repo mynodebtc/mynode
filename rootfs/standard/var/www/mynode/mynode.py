aa4f9c966ff14dcb1055fe534e0e912a17cd3b68
mynode-dev <dev@mynodebtc.com>
2022-02-09 23:20:37 -0600

    Set price update rate to 30s

--- rootfs/standard/var/www/mynode/mynode.py
+++ rootfs/standard/var/www/mynode/mynode.py
@@ -760,7 +760,7 @@ def start_threads():
     lnd_thread = BackgroundThread(update_lnd_info_thread, 60)
     lnd_thread.start()
     threads.append(lnd_thread)
-    price_thread = BackgroundThread(update_price_info_thread, 2) # 2 minutes
+    price_thread = BackgroundThread(update_price_info_thread, 30)
     price_thread.start()
     threads.append(price_thread)
     drive_thread = BackgroundThread(update_device_info, 60)
