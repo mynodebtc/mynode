937b3a72ad6deb10d1fd6b993e6e4ae275c2461c
mynode-dev <dev@mynodebtc.com>
2022-02-17 21:00:30 -0600

    Upgrade LiT to v0.6.3

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -22,7 +22,7 @@ POOL_LATEST_VERSION_FILE=/home/bitcoin/.mynode/pool_version_latest
 POOL_UPGRADE_MANIFEST_URL=https://github.com/lightninglabs/pool/releases/download/$POOL_VERSION/manifest-$POOL_VERSION.txt
 POOL_UPGRADE_MANIFEST_SIG_URL=https://github.com/lightninglabs/pool/releases/download/$POOL_VERSION/manifest-$POOL_VERSION.sig
 
-LIT_VERSION="v0.6.2-alpha"
+LIT_VERSION="v0.6.3-alpha"
 LIT_VERSION_FILE=/home/bitcoin/.mynode/lit_version
 LIT_LATEST_VERSION_FILE=/home/bitcoin/.mynode/lit_version_latest
 LIT_UPGRADE_MANIFEST_URL=https://github.com/lightninglabs/lightning-terminal/releases/download/$LIT_VERSION/manifest-$LIT_VERSION.txt
