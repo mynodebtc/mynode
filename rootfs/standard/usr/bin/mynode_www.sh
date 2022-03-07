38ee8b2ce67c7ed820d76f87efd0724a3ef80d58
Taylor Helsper <tehelsper@gmail.com>
2022-03-07 17:17:41 -0600

    Prefer python3 for web UI

--- rootfs/standard/usr/bin/mynode_www.sh
+++ rootfs/standard/usr/bin/mynode_www.sh
@@ -3,5 +3,14 @@
 if [ -f /home/bitcoin/.mynode/.www_use_python3 ]; then
     exec /usr/local/bin/python3 /var/www/mynode/mynode.py
 else
-    exec /usr/bin/python2.7 /var/www/mynode/mynode.py
+    # If not forcing python3, use prefer python3 unless in restart cycle
+    count=$(journalctl -b -u www.service | grep -c "Started myNode Web Server")
+    modcount=$(($count % 5))
+    if [ "$modcount" -eq 4 ]; then
+        echo "RESTART COUNT: $modcount (PYTHON2)"
+        exec /usr/bin/python2.7 /var/www/mynode/mynode.py
+    else
+        echo "RESTART COUNT: $modcount (PYTHON3)"
+        exec /usr/local/bin/python3 /var/www/mynode/mynode.py
+    fi
 fi
