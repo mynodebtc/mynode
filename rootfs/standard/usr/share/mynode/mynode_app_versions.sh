81afaef5042ad552accd826b3b294fd2daa95d66
Taylor Helsper <tehelsper@gmail.com>
2022-04-02 22:29:22 -0500

    Upgrade Sphinx Relay to v2.2.6

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -115,7 +115,7 @@ BOS_VERSION="11.57.0"
 BOS_VERSION_FILE=/home/bitcoin/.mynode/bos_version
 BOS_LATEST_VERSION_FILE=/home/bitcoin/.mynode/bos_version_latest
 
-SPHINXRELAY_VERSION="v2.2.5"
+SPHINXRELAY_VERSION="v2.2.6"
 SPHINXRELAY_VERSION_FILE=/home/bitcoin/.mynode/sphinxrelay_version
 SPHINXRELAY_LATEST_VERSION_FILE=/home/bitcoin/.mynode/sphinxrelay_version_latest
 
