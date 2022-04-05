499d81af3f281bfac2b6243363f4a1c9da2bdbe9
Taylor Helsper <tehelsper@gmail.com>
2022-04-04 19:11:11 -0500

    Upgrade Thunderhub to v0.13.10

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -95,7 +95,7 @@ SPECTER_VERSION="1.9.1"
 SPECTER_VERSION_FILE=/home/bitcoin/.mynode/specter_version
 SPECTER_LATEST_VERSION_FILE=/home/bitcoin/.mynode/specter_version_latest
 
-THUNDERHUB_VERSION="v0.13.7"
+THUNDERHUB_VERSION="v0.13.10"
 THUNDERHUB_VERSION_FILE=/home/bitcoin/.mynode/thunderhub_version
 THUNDERHUB_LATEST_VERSION_FILE=/home/bitcoin/.mynode/thunderhub_version_latest
 
