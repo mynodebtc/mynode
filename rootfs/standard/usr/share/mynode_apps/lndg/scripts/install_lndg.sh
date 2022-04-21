cba62f187637e86262d4758c9a0fa49c2dd01f11
Taylor Helsper <tehelsper@gmail.com>
2022-04-20 23:07:49 -0500

    Updates for sample dynamic app

---
+++ rootfs/standard/usr/share/mynode_apps/lndg/scripts/install_lndg.sh
@@ -0,0 +1,20 @@
+# Helpful variables to add:
+#   VERSION?
+#   DOWNLOAD PATH TO TAR.GZ?
+#   PATH TO SD CARD FOLDER
+#   PATH TO DATA DRIVE FOLDER
+#
+
+echo "INSTALLING LNDG - START"
+
+whoami
+id
+
+sleep 3s
+
+pwd
+ls -lsa
+
+sleep 3s
+
+echo "INSTALLING LNDG - END"
---
+++ rootfs/standard/usr/share/mynode_apps/lndg/scripts/uninstall_lndg.sh
@@ -0,0 +1,13 @@
+echo "UNINSTALLING LNDG - START"
+
+whoami
+id
+
+sleep 3s
+
+pwd
+ls -lsa
+
+sleep 3s
+
+echo "UNINSTALLING LNDG - END"
