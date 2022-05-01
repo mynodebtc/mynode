1e59d88c5adcfc3b17572080e8d77be1f937d4fb
Taylor Helsper <tehelsper@gmail.com>
2022-04-30 22:42:30 -0500

    Upgrade BoS to v12.7.0

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -118,7 +118,7 @@ CKBUNKER_VERSION="v0.9"
 CKBUNKER_VERSION_FILE=/home/bitcoin/.mynode/ckbunker_version
 CKBUNKER_LATEST_VERSION_FILE=/home/bitcoin/.mynode/ckbunker_version_latest
 
-BOS_VERSION="12.0.2"
+BOS_VERSION="12.7.0"
 BOS_VERSION_FILE=/home/bitcoin/.mynode/bos_version
 BOS_LATEST_VERSION_FILE=/home/bitcoin/.mynode/bos_version_latest
 
