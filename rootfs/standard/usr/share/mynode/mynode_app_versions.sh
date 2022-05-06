fe05fb28a214c52dc5a90aec7e22e344b62ab20d
mynode-dev <dev@mynodebtc.com>
2022-05-06 15:46:33 -0500

    Upgrade Thunderhub to v0.13.11

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -102,7 +102,7 @@ SPECTER_VERSION="1.9.4"
 SPECTER_VERSION_FILE=/home/bitcoin/.mynode/specter_version
 SPECTER_LATEST_VERSION_FILE=/home/bitcoin/.mynode/specter_version_latest
 
-THUNDERHUB_VERSION="v0.13.10"
+THUNDERHUB_VERSION="v0.13.11"
 THUNDERHUB_VERSION_FILE=/home/bitcoin/.mynode/thunderhub_version
 THUNDERHUB_LATEST_VERSION_FILE=/home/bitcoin/.mynode/thunderhub_version_latest
 
