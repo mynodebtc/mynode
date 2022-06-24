35d310c23cf1d6bb24de2445123f34b69148c3f2
Taylor Helsper <tehelsper@gmail.com>
2022-06-23 22:48:55 -0500

    Add dynamic app uninstall

--- rootfs/standard/var/www/mynode/static/js/mynode.js
+++ rootfs/standard/var/www/mynode/static/js/mynode.js
@@ -49,3 +49,63 @@ function showAlertPopup(divid, message) {
     $("#"+divid).html(message);
     $("#"+divid).show().delay(3000).fadeOut();
 }
+
+// Check if using tor
+function is_using_tor() {
+    if (location.hostname.includes(".onion")) {
+        return true;
+    }
+    return false;
+}
+
+// Open new tab
+function open_new_mynode_tab(port, protocol="same") {
+    hostname=location.hostname
+    if (protocol == "same") {
+        protocol = location.protocol
+    } else if (protocol == "http" || protocol == "https") {
+        protocol = protocol + ":"
+    }
+    url = protocol+'//'+hostname+':'+port
+    window.open(url,'_blank');
+}
+
+// Open app in new tab
+function open_app_in_new_tab(http_port, https_port="NA", requires_https=false, custom_tor_address="NA", tor_http_port="80", tor_https_port="443") {
+    protocol=location.protocol
+    hostname=location.hostname
+    port_string=""
+
+    if (is_using_tor() && custom_tor_address != "NA") {
+        hostname=custom_tor_address
+        // Use "default" port - either 80 or 443 for HTTP/HTTPS unless overriden
+        if (protocol == "http:" && tor_http_port != "80") {
+            port_string=":"+tor_http_port
+        }
+        if (protocol == "https:" && tor_https_port != "443") {
+            port_string=":"+tor_https_port
+        }
+
+        // If app is HTTP only
+        if (tor_https_port == "NA") {
+            protocol = "http:"
+            port_string = ":"+tor_http_port
+        }
+    } else {
+        if (protocol == "http:" && http_port != "80") {
+            port_string=":"+http_port
+        }
+        if (protocol == "https:" && https_port != "443") {
+            port_string=":"+https_port
+        }
+
+        // If app is HTTP only
+        if (https_port == "NA") {
+            protocol = "http:"
+            port_string = ":"+http_port
+        }
+    }
+    
+    url = protocol+'//'+hostname+port_string
+    window.open(url,'_blank');
+}
