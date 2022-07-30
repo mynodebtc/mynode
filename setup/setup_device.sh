7ff8930c9191dcff9cd5c9bfbef39696ce5c55e1
mynode-dev <dev@mynodebtc.com>
2022-07-30 17:08:49 -0500

    Add btrfs tools

--- setup/setup_device.sh
+++ setup/setup_device.sh
@@ -205,7 +205,7 @@ apt-get -y install openjdk-11-jre libevent-dev ncurses-dev
 apt-get -y install zlib1g-dev libudev-dev libusb-1.0-0-dev python3-venv gunicorn
 apt-get -y install sqlite3 libsqlite3-dev torsocks python3-requests libsystemd-dev
 apt-get -y install libjpeg-dev zlib1g-dev psmisc hexyl libbz2-dev liblzma-dev netcat-openbsd
-apt-get -y install hdparm iotop nut obfs4proxy libpq-dev socat
+apt-get -y install hdparm iotop nut obfs4proxy libpq-dev socat btrfs-tools
 
 # Install Openbox GUI
 if [ $IS_X86 = 1 ]; then
