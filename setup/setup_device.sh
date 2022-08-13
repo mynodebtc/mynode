4c81bd568ddc0a43cf05bc619ca9363415f8bc80
Taylor Helsper <tehelsper@gmail.com>
2022-08-12 22:27:46 -0500

    Add debian backports key

--- setup/setup_device.sh
+++ setup/setup_device.sh
@@ -184,6 +184,8 @@ curl https://samouraiwallet.com/pgp.txt | gpg --import # two keys from Samourai 
 gpg  --keyserver hkp://keyserver.ubuntu.com --recv-keys DE23E73BFA8A0AD5587D2FCDE80D2F3F311FD87E #loopd
 curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import  # tor
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -                                       # tor
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138     # Debian Backports
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9     # Debian Backports
 
 
 # Update OS
