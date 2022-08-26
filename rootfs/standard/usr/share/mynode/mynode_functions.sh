9700cf4f3b25d8ebe4e7e962e7f6a1b9306c1137
mynode-dev <dev@mynodebtc.com>
2022-08-25 23:04:41 -0500

    Add setting to remove backup dns servers

--- rootfs/standard/usr/share/mynode/mynode_functions.sh
+++ rootfs/standard/usr/share/mynode/mynode_functions.sh
@@ -10,6 +10,16 @@ function should_install_app {
     return 1
 }
 
+function settings_file_exists {
+    if [ -f /home/bitcoin/.mynode/${1} ]; then
+       return 0
+    fi
+    if [ -f /mnt/hdd/mynode/settings/${1} ]; then
+       return 0
+    fi
+    return 1
+}
+
 function skip_base_upgrades {
     if [ -f /tmp/skip_base_upgrades ]; then
         return 0
