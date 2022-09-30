094258cc2b39e3da805f67cde5939069d1e1eb7e
mynode-dev <dev@mynodebtc.com>
2022-09-29 22:35:37 -0500

    Upgrade JoininBox to v0.7.3

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -77,7 +77,7 @@ CORSPROXY_VERSION=$(get_app_version "$CORSPROXY_VERSION" "corsproxy")
 CORSPROXY_VERSION_FILE=/home/bitcoin/.mynode/corsproxy_version
 CORSPROXY_LATEST_VERSION_FILE=/home/bitcoin/.mynode/corsproxy_version_latest
 
-JOININBOX_VERSION="v0.7.2"
+JOININBOX_VERSION="v0.7.3"
 JOININBOX_VERSION=$(get_app_version "$JOININBOX_VERSION" "joininbox")
 JOININBOX_VERSION_FILE=/home/bitcoin/.mynode/joininbox_version
 JOININBOX_LATEST_VERSION_FILE=/home/bitcoin/.mynode/joininbox_version_latest
