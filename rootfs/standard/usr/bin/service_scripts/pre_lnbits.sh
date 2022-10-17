1f26ce66d424f8930f89c5063df4bac29f14f98b
mynode-dev <dev@mynodebtc.com>
2022-10-16 22:23:48 -0500

    Make lnbits use macaroon path rather than hex

--- rootfs/standard/usr/bin/service_scripts/pre_lnbits.sh
+++ rootfs/standard/usr/bin/service_scripts/pre_lnbits.sh
@@ -4,9 +4,7 @@ set -x
 
 
 # Generate hex macaroons
-macaroonAdminHex=$(xxd -ps -u -c 1000 /mnt/hdd/mynode/lnd/data/chain/bitcoin/mainnet/admin.macaroon)
+#macaroonAdminHex=$(xxd -ps -u -c 1000 /mnt/hdd/mynode/lnd/data/chain/bitcoin/mainnet/admin.macaroon)
 
 # Update env file
-sed -i "s/^LND_REST_MACAROON=.*/LND_REST_MACAROON=${macaroonAdminHex}/g" /opt/mynode/lnbits/.env
-#sed -i "s/^LND_REST_INVOICE_MACAROON=.*/LND_REST_INVOICE_MACAROON=${macaroonInvoiceHex}/g" /opt/mynode/lnbits/.env
-#sed -i "s/^LND_REST_READ_MACAROON=.*/LND_REST_READ_MACAROON=${macaroonReadHex}/g" /opt/mynode/lnbits/.env
+sed -i "s|^LND_REST_MACAROON=.*|LND_REST_MACAROON=\"/app/admin.macaroon\"|g" /opt/mynode/lnbits/.env
