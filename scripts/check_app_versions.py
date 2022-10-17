5c0b07e5717cd3ea578f9699206cea3b29b9abe1
Taylor Helsper <tehelsper@gmail.com>
2022-10-16 21:57:50 -0500

    Add jam to version check script

--- scripts/check_app_versions.py
+++ scripts/check_app_versions.py
@@ -16,6 +16,7 @@ apps = [{"name": "bitcoin/bitcoin",                         "current_version_var
         {"name": "BlueWallet/LndHub",                       "current_version_variable": "LNDHUB_VERSION"},
         {"name": "btcpayserver/btcpayserver",               "current_version_variable": "BTCPAYSERVER_VERSION"},
         {"name": "openoms/joininbox",                       "current_version_variable": "JOININBOX_VERSION"},
+        {"name": "joinmarket-webui/jam",                    "dynamic_app_name":         "jam"},
         {"name": "unchained-capital/caravan",               "current_version_variable": "CARAVAN_VERSION"},
         {"name": "cryptoadvance/specter-desktop",           "current_version_variable": "SPECTER_VERSION"},
         {"name": "Coldcard/ckbunker",                       "current_version_variable": "CKBUNKER_VERSION"},
