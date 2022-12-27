fb44a229a2917face115d4180c77867546289427
mynode-dev <dev@mynodebtc.com>
2022-12-26 22:48:56 -0600

    Add Astral App

---
+++ rootfs/standard/usr/share/mynode_apps/astral/scripts/uninstall_astral.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /usr/share/mynode/mynode_device_info.sh
+source /usr/share/mynode/mynode_app_versions.sh
+
+echo "==================== UNINSTALLING APP ===================="
+
+# The app folder will be removed automatically after this script runs. You may not need to do anything here.
+
+# TODO: Perform special uninstallation steps here
+
+echo "================== DONE UNINSTALLING APP ================="
