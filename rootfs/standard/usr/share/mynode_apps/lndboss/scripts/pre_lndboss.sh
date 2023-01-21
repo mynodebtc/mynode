3c0ca4c7acec6db04d545d0bd0997fe29722f0ba
mynode-dev <dev@mynodebtc.com>
2023-01-21 00:12:21 -0600

    Change LndBoss install process

--- rootfs/standard/usr/share/mynode_apps/lndboss/scripts/pre_lndboss.sh
+++ rootfs/standard/usr/share/mynode_apps/lndboss/scripts/pre_lndboss.sh
@@ -12,3 +12,24 @@ if [ -f /home/bitcoin/.mynode/.hashedpw_bcrypt ]; then
 }
 EOF
 fi
+
+mkdir -p /mnt/hdd/mynode/lndboss/local
+cat << EOF > /mnt/hdd/mynode/lndboss/config.json
+{
+  "default_saved_node": "local"
+}
+EOF
+cat << EOF > /mnt/hdd/mynode/lndboss/local/credentials.json
+{
+  "cert_path": "/.lnd/tls.cert",
+  "macaroon_path": "/.lnd/data/chain/bitcoin/mainnet/admin.macaroon",
+  "socket": "host.docker.internal:10009"
+}
+EOF
+
+
+# Create env file
+MY_UID=$(id -u)
+MY_GID=$(id -g)
+echo "UID=$MY_UID" >  /mnt/hdd/mynode/lndboss/env
+echo "GID=$MY_GID" >> /mnt/hdd/mynode/lndboss/env
