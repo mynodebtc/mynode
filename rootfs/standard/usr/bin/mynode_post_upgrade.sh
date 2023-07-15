0a9056fec0ee4f2c4561415998335fa42e96dd13
mynode-dev <dev@mynodebtc.com>
2023-07-14 21:14:44 -0500

    Add docker-buildx-plugin to resolve docker image build issues

--- rootfs/standard/usr/bin/mynode_post_upgrade.sh
+++ rootfs/standard/usr/bin/mynode_post_upgrade.sh
@@ -325,7 +325,7 @@ if ! skip_base_upgrades ; then
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     $TORIFY apt-get update --allow-releaseinfo-change
-    $TORIFY apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin || true
+    $TORIFY apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin || true
     
     # Use systemd for managing Docker
     rm -f /etc/init.d/docker
