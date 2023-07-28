4b39a54d4113c4d7df757a4a2d4499dc429fee48
mynode-dev <dev@mynodebtc.com>
2023-07-27 23:57:21 -0500

    Upgrade LNBits to v0.10.9

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -124,7 +124,7 @@ BTCRPCEXPLORER_VERSION=$(get_app_version "$BTCRPCEXPLORER_VERSION" "btcrpcexplor
 BTCRPCEXPLORER_VERSION_FILE=/home/bitcoin/.mynode/btcrpcexplorer_version
 BTCRPCEXPLORER_LATEST_VERSION_FILE=/home/bitcoin/.mynode/btcrpcexplorer_version_latest
 
-LNBITS_VERSION="0.10.6"
+LNBITS_VERSION="0.10.9"
 LNBITS_VERSION=$(get_app_version "$LNBITS_VERSION" "lnbits")
 LNBITS_VERSION_FILE=/home/bitcoin/.mynode/lnbits_version
 LNBITS_LATEST_VERSION_FILE=/home/bitcoin/.mynode/lnbits_version_latest
