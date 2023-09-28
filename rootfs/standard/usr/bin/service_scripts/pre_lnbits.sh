fac821baf0b3bc33561663e146e5ba74b58586c8
Taylor Helsper <tehelsper@gmail.com>
2023-09-27 21:02:49 -0500

    Add code to update lnbits settings file (commented out)

--- rootfs/standard/usr/bin/service_scripts/pre_lnbits.sh
+++ rootfs/standard/usr/bin/service_scripts/pre_lnbits.sh
@@ -8,6 +8,14 @@ if [ ! -f /mnt/hdd/mynode/lnbits/.env ]; then
     chown bitcoin:bitcoin /mnt/hdd/mynode/lnbits/.env
 fi
 
+# Force update of LNBits config file (increment to force new update)
+# LNBITS_CONFIG_UPDATE_NUM=1
+# if [ ! -f /mnt/hdd/mynode/lnbits/update_config_$LNBITS_CONFIG_UPDATE_NUM ]; then
+#     cp -f /usr/share/mynode/lnbits.env /mnt/hdd/mynode/lnbits/.env
+#     chown bitcoin:bitcoin /mnt/hdd/mynode/lnbits/.env
+#     touch /mnt/hdd/mynode/lnbits/update_config_$LNBITS_CONFIG_UPDATE_NUM
+# fi
+
 # Generate hex macaroons
 #macaroonAdminHex=$(xxd -ps -u -c 1000 /mnt/hdd/mynode/lnd/data/chain/bitcoin/mainnet/admin.macaroon)
 
