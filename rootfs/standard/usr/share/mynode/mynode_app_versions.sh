665872282f56bf6c02b48447bf1e1011e6103cbe
fanquake <fanquake@gmail.com>
2023-10-23 16:52:42 +0100

    Bitcoin Core v25.1
    Rel notes:
    https://github.com/bitcoin/bitcoin/blob/master/doc/release-notes/release-notes-25.1.md.

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -16,7 +16,7 @@ function get_app_version()
     echo "$version"
 }
 
-BTC_VERSION="25.0"
+BTC_VERSION="25.1"
 BTC_VERSION=$(get_app_version "$BTC_VERSION" "bitcoin")
 BTC_VERSION_FILE=/home/bitcoin/.mynode/bitcoin_version
 BTC_LATEST_VERSION_FILE=/home/bitcoin/.mynode/bitcoin_version_latest
