0be0181ed88a2533b40f722b21875aa69d6cea5f
Taylor Helsper <tehelsper@gmail.com>
2023-11-05 16:25:50 -0600

    Fix issue parsing lncli output

--- rootfs/standard/usr/bin/wait_on_lnd.sh
+++ rootfs/standard/usr/bin/wait_on_lnd.sh
@@ -9,11 +9,11 @@ fi
 
 # Wait until lnd is synced
 echo "Checking if LND is synced..."
-lncli --network=$NETWORK --lnddir /mnt/hdd/mynode/lnd getinfo | grep 'synced_to_chain": true'
+lncli --network=$NETWORK --lnddir /mnt/hdd/mynode/lnd getinfo | grep 'synced_to_chain":\s*true'
 while [ $? -ne 0 ]; do
     echo "LND not synced, sleeping for 60 seconds..."
     /bin/sleep 60s
-    lncli --network=$NETWORK --lnddir /mnt/hdd/mynode/lnd getinfo | grep 'synced_to_chain": true'
+    lncli --network=$NETWORK --lnddir /mnt/hdd/mynode/lnd getinfo | grep 'synced_to_chain":\s*true'
 done
 echo "LND is ready."
 exit 0
