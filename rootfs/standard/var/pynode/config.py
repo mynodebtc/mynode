561d0c5b009c5ce7304646b957e20ffe345a2811
mynode-dev <dev@mynodebtc.com>
2023-12-15 15:03:27 -0600

    Add flag to track if bitcoin has ever fully synced

--- rootfs/standard/var/pynode/config.py
+++ rootfs/standard/var/pynode/config.py
@@ -6,6 +6,7 @@ CHECKIN_URL = "https://www.mynodebtc.com/device_api/check_in.php"
 # Bitcoin Variables
 BITCOIN_ENV_FILE = "/mnt/hdd/mynode/bitcoin/env"
 BITCOIN_SYNCED_FILE = "/mnt/hdd/mynode/.mynode_bitcoin_synced"
+BITCOIN_SYNCED_AT_LEAST_ONCE="/mnt/hdd/mynode/.mynode_bitcoin_synced_at_least_once"
 
 # LND Variables
 LND_DATA_FOLDER = "/mnt/hdd/mynode/lnd/data/"
