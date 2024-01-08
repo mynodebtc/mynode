5dd355c3642535d0c23d7cd1067b9d5db20f5a33
Taylor Helsper <tehelsper@gmail.com>
2024-01-07 18:56:18 -0600

    Add new make target for raspi5

---
+++ scripts/setup_new_raspi5.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOCAL_IP=$(python3 ./scripts/get_local_ip.py)
+
+echo ""
+echo "Finished updating rootfs and other files!"
+echo ""
+
+clear
+echo "Step 1: "
+echo "  Flash SD Card with Raspberry Pi Image: out/base_images/raspi5_base.img.gz"
+echo "  OR"
+echo "  If you would rather create your own base image, follow the"
+echo "  manual instructions in doc/setup_base_image_raspi5.md"
+echo ""
+echo "Press a key when complete..."
+read -n 1
+
+clear
+echo "Step 2: "
+echo "  Insert SD Card into your Raspberry Pi and boot the device"
+echo ""
+echo "Press a key when complete..."
+read -n 1
+
+clear
+echo "Step 3: "
+echo "  Login to device with username 'admin' and password 'bolt'"
+echo "  Run the following commands. Use bolt at password prompts."
+echo "    wget http://${LOCAL_IP}:8000/setup_device.sh -O setup_device.sh"
+echo "    chmod +x setup_device.sh"
+echo "    sudo ./setup_device.sh ${LOCAL_IP}"
+echo ""
+echo "Press a key when complete..."
+read -n 1
+
+clear
+echo "Step 4:"
+echo "  Reboot your device."
+echo ""
+echo "Press a key when complete..."
+read -n 1
+
+clear
+echo "Congratulations! Your device is now ready!"
+echo "  Access it via a web browser at http://mynode.local/ or http://<device ip>/"
+echo "  Access it via SSH using the default credentials: admin / bolt"
+echo "  You should change your password on the settings page in the web GUI"
+echo ""
