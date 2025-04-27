687604b2ccd531b9e203144af16ce06a278952f9
tlindi <tomi@lind.fi>
2025-04-27 23:04:54 +0300

    Update MyNodeBTC mempool-docker-compose.yml
    present as comments the changes on mempool v3.0.0 .yml
    New composer creates containers using "-" as a separator instead of "_".
    See: https://docs.docker.com/compose/releases/migrate

--- rootfs/standard/usr/bin/service_scripts/post_mempool.sh
+++ rootfs/standard/usr/bin/service_scripts/post_mempool.sh
@@ -12,21 +12,21 @@ isRunning=""
 # check on loop if  mysql db is running. when running initialize
 while [ 1 ]; do
   # Check if mempool mysql db is running (check the db container)
-  isRunning=$(docker inspect --format="{{.State.Running}}" mempool_db_1)
+  isRunning=$(docker inspect --format="{{.State.Running}}" mempool-db-1)
   if [ "$isRunning" == "true" ]; then
     sleep 5s
 
     # Initialize database
-    databases=$(docker exec -i mempool_db_1 mysql -uroot -padmin -e "SHOW DATABASES;")
+    databases=$(docker exec -i mempool-db-1 mysql -uroot -padmin -e "SHOW DATABASES;")
     if [[ "$databases" == *"information_schema"* ]]; then   # Check DB is responding
         if [[ "$databases" == *"mempool"* ]]; then
             # DB found, exit 0
             exit 0
         else
             # Setup a database for mempool
-            $(docker exec -i mempool_db_1 mysql -uroot -padmin -e "drop database mempool;")
-            $(docker exec -i mempool_db_1 mysql -uroot -padmin -e "create database mempool;")
-            $(docker exec -i mempool_db_1 mysql -uroot -padmin -e "grant all privileges on mempool.* to 'mempool'@'%' identified by 'mempool';")
+            $(docker exec -i mempool-db-1 mysql -uroot -padmin -e "drop database mempool;")
+            $(docker exec -i mempool-db-1 mysql -uroot -padmin -e "create database mempool;")
+            $(docker exec -i mempool-db-1 mysql -uroot -padmin -e "grant all privileges on mempool.* to 'mempool'@'%' identified by 'mempool';")
             exit 0
         fi
     fi
