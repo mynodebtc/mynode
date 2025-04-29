2e74d7cbf89f18eb6757121c3acbc4a39e520399
tlindi <tomi@lind.fi>
2025-04-29 22:23:34 +0300

    Update post_mempool.sh
    supports both v1 and v2 docker-compose

--- rootfs/standard/usr/bin/service_scripts/post_mempool.sh
+++ rootfs/standard/usr/bin/service_scripts/post_mempool.sh
@@ -6,27 +6,38 @@ source /usr/share/mynode/mynode_config.sh
 
 sleep 10s
 
-# initalize mysql db (REQUIRED TO START MYSQL)
+# Check Docker Compose version
+compose_version=$(docker-compose --version)
+
+if [[ $compose_version == *"v2."* ]]; then
+  separator="-"
+else
+  separator="_"
+fi
+
+export DB_CONTAINER_NAME="mempool${separator}db${separator}1"
+
+# initialize mysql db (REQUIRED TO START mysql)
 isRunning=""
 
-# check on loop if  mysql db is running. when running initialize
+# Check on loop if mysql db is running; when running, initialize
 while [ 1 ]; do
   # Check if mempool mysql db is running (check the db container)
-  isRunning=$(docker inspect --format="{{.State.Running}}" mempool-db-1)
+  isRunning=$(docker inspect --format="{{.State.Running}}" $DB_CONTAINER_NAME)
   if [ "$isRunning" == "true" ]; then
     sleep 5s
 
     # Initialize database
-    databases=$(docker exec -i mempool-db-1 mysql -uroot -padmin -e "SHOW DATABASES;")
+    databases=$(docker exec -i $DB_CONTAINER_NAME mysql -uroot -padmin -e "SHOW DATABASES;")
     if [[ "$databases" == *"information_schema"* ]]; then   # Check DB is responding
         if [[ "$databases" == *"mempool"* ]]; then
             # DB found, exit 0
             exit 0
         else
             # Setup a database for mempool
-            $(docker exec -i mempool-db-1 mysql -uroot -padmin -e "drop database mempool;")
-            $(docker exec -i mempool-db-1 mysql -uroot -padmin -e "create database mempool;")
-            $(docker exec -i mempool-db-1 mysql -uroot -padmin -e "grant all privileges on mempool.* to 'mempool'@'%' identified by 'mempool';")
+            $(docker exec -i $DB_CONTAINER_NAME mysql -uroot -padmin -e "drop database mempool;")
+            $(docker exec -i $DB_CONTAINER_NAME mysql -uroot -padmin -e "create database mempool;")
+            $(docker exec -i $DB_CONTAINER_NAME mysql -uroot -padmin -e "grant all privileges on mempool.* to 'mempool'@'%' identified by 'mempool';")
             exit 0
         fi
     fi
