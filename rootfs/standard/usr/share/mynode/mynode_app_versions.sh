99a74d6dfd04bcfd670ffd523ab29749f78da673
Taylor Helsper <tehelsper@gmail.com>
2025-05-12 20:29:58 -0500

    Upgrade Specter to v2.1.1; Fix Specter issue with Bitcoin 28.1

--- rootfs/standard/usr/share/mynode/mynode_app_versions.sh
+++ rootfs/standard/usr/share/mynode/mynode_app_versions.sh
@@ -143,7 +143,10 @@ LNBITS_VERSION=$(get_app_version "$LNBITS_VERSION" "lnbits")
 LNBITS_VERSION_FILE=/home/bitcoin/.mynode/lnbits_version
 LNBITS_LATEST_VERSION_FILE=/home/bitcoin/.mynode/lnbits_version_latest
 
-SPECTER_VERSION="1.14.1"
+SPECTER_VERSION="2.1.1"
+if [ "$DEBIAN_VERSION" -lt "12" ]; then
+    SPECTER_VERSION="1.14.1"
+fi
 SPECTER_VERSION=$(get_app_version "$SPECTER_VERSION" "specter")
 SPECTER_VERSION_FILE=/home/bitcoin/.mynode/specter_version
 SPECTER_LATEST_VERSION_FILE=/home/bitcoin/.mynode/specter_version_latest
