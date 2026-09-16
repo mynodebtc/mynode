e82e7404a7c06089e7f0e660b13c54e699d1d9cc
mynode-dev <dev@mynodebtc.com>
2026-09-15 21:30:58 -0500

    Fix USB driver check regex

--- rootfs/standard/usr/bin/mynode_usb_driver_check.py
+++ rootfs/standard/usr/bin/mynode_usb_driver_check.py
@@ -147,7 +147,7 @@ def get_quirks_from_uas_devices():
         for line in lsusb_lines:
             try:
                 if "SATA 6Gb/s bridge" in line or "NVME" in line:
-                    m = re.search("Bus ([0-9]+) Device ([0-9]+): ID (\S+)", line)
+                    m = re.search(r"Bus ([0-9]+) Device ([0-9]+): ID (\S+)", line)
                     bus = m.group(1)
                     dev = m.group(2)
                     id = m.group(3)
